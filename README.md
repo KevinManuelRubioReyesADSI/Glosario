e679b455d85f33fcfa07c921551c19af3f3d3b5f
KevinManuelRubioReyesADSI <kevinsolo909@gmail.com>
2022-10-10 16:28:03 -0500

    map

--- README.md
+++ README.md
@@ -14,3 +14,16 @@ function myFunction(item) {
 
 ------
 
+## map
+
+```js
+let arr = [2, 3, 5, 7]
+
+arr.map(function(element, index, array){
+    document.getElementById("element").innerHTML = element;
+    document.getElementById("index").innerHTML = index;
+    document.getElementById("array").innerHTML = array;
+    return element;
+}, 80);
+```
+
--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-let sum = 0;
-const numbers = [65, 44, 12, 4];
-numbers.forEach(myFunction);
+let arr = [2, 3, 5, 7]
 
-document.getElementById("demo").innerHTML = sum;
-
-function myFunction(item) {
-  sum += item;
-}
+arr.map(function(element, index, array){
+    document.getElementById("element").innerHTML = element;
+    document.getElementById("index").innerHTML = index;
+    document.getElementById("array").innerHTML = array;
+    return element;
+}, 80);
